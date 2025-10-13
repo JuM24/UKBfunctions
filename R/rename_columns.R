a6c9609fee07b2c61fab2004057ecbc550fd3cbc
Jure MUR <jure.mur@uni.lu>
2025-10-13 14:47:16 +0200

    Add name replacement function.

---
+++ R/rename_columns.R
@@ -0,0 +1,42 @@
+#' Rename columns based on dictionary
+#'
+#' `rename_columns` takes a data frame and a .csv file that acts as a dictionary
+#' to change column names of the data frame.
+#' @param df Input data frame whose column names will be changed.
+#' @param colname_file Data frame that contains as separate columns both old
+#' and new column names of `df`.
+#' @param old_cols Name of the column with old column names.
+#' @param new_cols Name of the column with new column names
+#' @export
+
+rename_columns <- function(df,
+                           colname_file,
+                           old_cols,# vector, same with new_cols
+                           new_cols){
+  # build lookup vector
+  name_dict <- setNames(old_cols, new_cols)
+  # list of simple names to simplify column name later on
+  new_names <- c()
+  new_names_whole <- c()
+  # rename iteratively
+  for (name in colnames(df)){
+    # main part of name
+    name_simple <- sub('^X([^.]*)\\..*$', '\\1', name)
+
+    # the instance suffix
+    suffix = str_replace(name, '^[^.]*', "")
+    # fetch new name from dictionary
+    new_name <- names(name_dict[name_dict == name_simple])
+    new_names <- c(new_names, new_name)
+    # paste together new name and replace old with new
+    new_name_whole <- paste0(new_name, suffix)
+    colnames(df)[which(colnames(df) == name)] <- new_name_whole
+    new_names_whole <- c(new_names_whole, new_name_whole)
+  }
+  # select indices of IDs with just a single instance
+  simple_ids <- which(new_names %in% names(table(new_names)[table(new_names) == 1]))
+  # use the indices above to further simplify those IDs
+  colnames(df)[simple_ids] <- new_names[simple_ids]
+
+  return(df)
+}
