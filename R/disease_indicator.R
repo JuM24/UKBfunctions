df0d589e1ccd02c3e60a78d8b31a19a439bc206e
Jure MUR <jure.mur@uni.lu>
2026-03-10 11:24:55 +0100

    Add the option of setting invalid dates to NA.

--- R/disease_indicator.R
+++ R/disease_indicator.R
@@ -8,6 +8,8 @@
 #' @param df Input data frame.
 #' @param colname Character; Name of the column with the dates for the event.
 #' @param date_form Character; format of the date.
+#' @param invalid_dates Array; strings of invalid dates in the form `date_form`.
+#' Events with these dates will be converted to NA.
 #' @param threshold_date Single character or vector of same length as `nrow(df)`;
 #'  should be in the same format as `colname`. If not `NULL`, events occurring
 #'  after this date are considered non-events and changed to NA in the returned
@@ -17,6 +19,7 @@
 disease_indicator <- function(df,
                               colname,
                               date_form = NULL,
+                              invalid_dates = NULL,
                               threshold_date = NULL){
 
   # to date type
@@ -37,6 +40,14 @@ disease_indicator <- function(df,
     if (!is.finite(threshold_date)) threshold_date <- as.Date(NA)
   }
 
+  # set invalid dates to NA
+  invalid_rows <- rep(FALSE, nrow(df))
+  if (!is.null(invalid_dates)) {
+    invalid_dates <- as.Date(invalid_dates, format = date_form)
+    invalid_rows <- df[[colname]] %in% invalid_dates
+    df[invalid_rows, colname] <- NA
+  }
+
   # add '_date' suffix to column name
   col_j <- which(colnames(df) == colname)
   if (length(col_j) == 0) stop('`colname` not found in `df`.')
@@ -45,7 +56,9 @@ disease_indicator <- function(df,
   colnames(df)[col_j] <- date_colname
 
   # create a separate indicator column; all dates after threshold date do not count
+  # indicators for invalid dates are set to NA
   df[[colname]] <- 0L
+  df[invalid_rows, colname] <- NA
   df[!is.na(df[[date_colname]]) &
         df[[date_colname]] <= threshold_date, colname] <- 1L
   df[!is.na(df[[date_colname]]) &
